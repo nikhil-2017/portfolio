262486ac6897176ff639e26aef44192aa822ccc8
Nikhil Gaikwad <nickgaikwad11@gmail.com>
2024-06-29 02:18:07 +0530

    Add education page.

--- src/config/portfolio/education.js
+++ src/config/portfolio/education.js
@@ -1,6 +1,19 @@
 export default {
   title: 'EDUCATION',
-  be: {},
-  diploma: {},
-  ssc: {}
+  items: {
+    be: {
+      name: "Vivekanand Education Society's Institute of Technology (V.E.S.I.T.)",
+      link: 'https://vesit.ves.ac.in/',
+      image: '/images/portfolio/education/vesit.jfif',
+      date: '2018 - 2021',
+      percentage: '> 9 Pointer'
+    },
+    diploma: {
+      name: 'Government Polytechnic Mumbai',
+      link: 'https://gpmumbai.ac.in/gpmweb/',
+      image: '/images//portfolio/education/gpm.png',
+      date: '2015 - 2018',
+      percentage: '88.26%'
+    }
+  }
 }
--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,9 +1,34 @@
 <template>
     <div>
-        <div class="grid">
-            <div class="col-12 sub-heading">EDUCATION</div>
+        <div class="grid mb-5 md:mb-0">
+            <div class="col-12 sub-heading">{{ Education.title }}</div>
+        </div>
+        <div class="grid justify-content-center mb-2" v-for="(item, index) in Education.items"
+            :key="`education-${index}`">
+            <div class="col-12 md:col-8 lg:col-6">
+                <Card>
+                    <template #content>
+                        <div class="grid">
+                            <div class="col-3 my-auto">
+                                <img class="img-fluid" :src="item.image" :alt="item.name">
+                            </div>
+                            <div class="col-9 my-auto">
+                                <div class="grid">
+                                    <div class="col-12 name"><a class="link" target="_blank" :href="item.link">{{
+                                        item.name }}</a></div>
+                                </div>
+                                <div class="grid">
+                                    <div class="col-6">{{ item.date }}</div>
+                                    <div class="col-6 text-right">{{ item.percentage }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+                </Card>
+            </div>
         </div>
     </div>
 </template>
 <script setup>
+import Education from "@/config/portfolio/education.js";
 </script>
