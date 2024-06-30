40b9f4299a811267e964cc3abe7120ef07e7ccd2
Nikhil Gaikwad <nickgaikwad11@gmail.com>
2024-06-30 15:38:38 +0530

    Add work experience feature.

--- src/config/portfolio/work_experience.js
+++ src/config/portfolio/work_experience.js
@@ -1,1 +1,44 @@
-export default {}
+export default {
+  title: 'WORK EXPERIENCE',
+  companies: {
+    network_marvels: {
+      name: 'NETWORK MARVELS',
+      link: 'https://networkmarvels.com',
+      target: '_blank',
+      job_title: 'Software Engineer',
+      image: '/images/portfolio/work_experience/network_marvels.png',
+      date: 'June 2021 - Present',
+      projects: [
+        'Developed a sophisticated learning platform designed to provide interactive events and sandbox environments. Implemented functionalities for deploying lab virtual machines (VMs), enabling users to gain hands-on experience with various technologies. Facilitated the creation of virtual environments where users can experiment and learn through practical exercises. Technologies Used: Utilized Vue 3 for the frontend development, Spring boot for the backend, and PostgreSql for the database management.',
+        'Developed a Line Card Application designed to showcase vendors and their products categorized efficiently, helping users to find the best products suited to their needs. This application enhances the user experience by organizing products by category, facilitating easy navigation and comparison. Technologies Used: Utilized Vue 3 for the frontend development, Laravel for the backend, and MySQL for the database management.'
+      ]
+    },
+    om_screen_art: {
+      name: 'OM SCREEN ART',
+      job_title: 'Software Developer',
+      image: '/images/portfolio/work_experience/om_screen_art.jpg',
+      date: '2024',
+      projects: [
+        'Developed a comprehensive Bill Management Application that efficiently manages bills and ledger transactions. The application offers a user-friendly interface for creating, tracking, and managing bills, as well as maintaining detailed ledger transactions. Key functionalities include bill creation, payment tracking, and ledger updates. Technologies Used: Utilized Vue 3 for the frontend development, Laravel for the backend, and MySQL for the database management.'
+      ]
+    }
+  },
+  carousel: {
+    network_marvels: {
+      name: 'Network Marvels',
+      image: '/images/portfolio/work_experience/network_marvels.png'
+    },
+    om_screen_art: {
+      name: 'OM Screen Art',
+      image: '/images/portfolio/work_experience/om_screen_art.jpg'
+    },
+    aceng: {
+      name: 'AceNG',
+      image: '/images/portfolio/work_experience/ace-ng.jpg'
+    },
+    portfolio: {
+      name: 'AceNG Portfolio',
+      image: '/images/portfolio/work_experience/ace-ng-portfolio.jpg'
+    }
+  }
+}
--- src/views/WorkExperience.vue
+++ src/views/WorkExperience.vue
@@ -1,15 +1,54 @@
-<template lang="">
+<template>
     <div>
-        <div class="grid">
-            <div class="col-12 sub-heading">WORK EXPERIENCE</div>
+        <div class="grid mb-5">
+            <div class="col-12 sub-heading">{{ WorkExperience.title }}</div>
+        </div>
+        <div class="grid mb-5 justify-content-center">
+            <div class="col-12 md:col-8 mb-5" v-for="(work, index) in WorkExperience.companies" :key="`work-${index}`">
+                <Card>
+                    <template #content>
+                        <div class="grid">
+                            <div class="col-12">
+                                <a v-if="work.link" :href="work.link" :target="(work?.target) ? work.target : '_self'">
+                                    <img class="img-fluid" :src="work.image" :alt="work.name">
+                                </a>
+                                <img v-else class="img-fluid" :src="work.image" :alt="work.name">
+                            </div>
+                        </div>
+                        <div class="grid">
+                            <div class="col-6">{{ work.job_title }}</div>
+                            <div class="col-6 text-right">{{ work.date }}</div>
+                        </div>
+                        <div class="grid">
+                            <div class="col-12">
+                                <ul class="list-none" style="padding-inline-start: .75rem;">
+                                    <li class="mb-3" v-for="(project, index) in work.projects"
+                                        :key="`project-${index}`">
+                                        <div v-html="project"></div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </template>
+                </Card>
+            </div>
+        </div>
+        <div class="grid mb-5">
+            <div class="col-12">
+                <div class="logos">
+                    <div class="logo-slide">
+                        <img v-for="(company, index) in WorkExperience.carousel" :key="`work-company-${index}`"
+                            :src="company.image" :alt="company.name">
+                    </div>
+                    <div class="logo-slide">
+                        <img v-for="(company, index) in WorkExperience.carousel" :key="`work-company-${index}`"
+                            :src="company.image" :alt="company.name">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
-<script>
-export default {
-
-}
+<script setup>
+import WorkExperience from '@/config/portfolio/work_experience.js'
 </script>
-<style lang="">
-
-</style>
